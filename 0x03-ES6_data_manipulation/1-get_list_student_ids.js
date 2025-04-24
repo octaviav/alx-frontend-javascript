a4c034d626f2a07264303b5aa3300c89f090440b
octaviav <octavia7104@gmail.com>
2025-04-24 10:40:56 +0000

    Mapping update

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(listStudents) {
-	if (!Array.isArray(listStudents)) {
-		return [];
-	}
-
-	return listStudents.map((student) => student.id);
+  if (!Array.isArray(listStudents)) {
+    return [];
+  }
+  
+  return listStudents.map((student) => student.id);
 }
